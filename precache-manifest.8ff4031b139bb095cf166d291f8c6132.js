6a2de97e5b3ec1709351e68e0df1767816097337
hafizmuhammadshoaib <shoaibsilat9@gmail.com>
2021-12-23 15:13:14 +0000

    Deploying to gh-pages from  @ a39c11afb9f92788f63daba4ddde672f994cf183 🚀

---
+++ precache-manifest.8ff4031b139bb095cf166d291f8c6132.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e15af15a752e78244ceb8f95f263a4e",
+    "url": "/developerfolio/index.html"
+  },
+  {
+    "revision": "072e0b65331d0a3add22",
+    "url": "/developerfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/developerfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "0c3abb0434646f5e0114",
+    "url": "/developerfolio/static/css/main.78532689.chunk.css"
+  },
+  {
+    "revision": "2317a7662f802fe0443f",
+    "url": "/developerfolio/static/js/2.7eb4529a.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerfolio/static/js/2.7eb4529a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072e0b65331d0a3add22",
+    "url": "/developerfolio/static/js/3.5bc25179.chunk.js"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/developerfolio/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "0c3abb0434646f5e0114",
+    "url": "/developerfolio/static/js/main.f16b6a20.chunk.js"
+  },
+  {
+    "revision": "1469477fc7fc0fe3356d",
+    "url": "/developerfolio/static/js/runtime-main.736db6bc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "9249a8ee1a93999c3fda3ddba898593f",
+    "url": "/developerfolio/static/media/fenix.9249a8ee.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "3113b784df3c3ae30a33c11e3119b68f",
+    "url": "/developerfolio/static/media/hamdard-university.3113b784.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
